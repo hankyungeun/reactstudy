f9614cad78cadfbc0c026b416f5a704b4fdcbd28
hangyeong-eun <az2282@naver.com>
2023-11-16 16:39:52 +0900

    chapter04 시계 만들기

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import Library  from './chapter03/Library';
+import Clock  from './chapter04/Clock';
 
+setInterval(() => {
 ReactDOM.render(
   <React.StrictMode>
-    <Library />
+    <Clock />
   </React.StrictMode>,
   document.getElementById('root')
 );
+},1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
