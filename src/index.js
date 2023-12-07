c39f542fd7c3d44e8b2a4c18c4b0b501e1bc09ae
hangyeong-eun <az2282@naver.com>
2023-12-07 21:52:31 +0900

    chapter10 map()사용해보기

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -11,12 +11,13 @@ import NotificationList from './chapter06/NotificationList';
 import Accommodate  from './chapter07/Accommodate';
 import ConfirmButton  from './chapter08/ConfirmButton';
 import LandingPage  from './chapter09/LandingPage';
+import AttendanceBook  from './chapter10/AttendanceBook';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <LandingPage />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <AttendanceBook />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
